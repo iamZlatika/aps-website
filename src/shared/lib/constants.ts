4ddd73940c783057f1ca8f1c754c00a93db98832
iamzlatika <hrynko.zlata@gmail.com>
2026-03-30 15:05:40 +0300

    Add theme swich

--- src/shared/lib/constants.ts
+++ src/shared/lib/constants.ts
@@ -1,16 +1,1 @@
-import { type Language } from "@/shared/types.ts";
-
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-export const DEFAULT_LANGUAGES: {
-  backoffice: Language;
-  client: Language;
-} = {
-  backoffice: (import.meta.env.VITE_BACKOFFICE_LANGUAGE as Language) || "ru",
-  client: (import.meta.env.VITE_CLIENT_LANGUAGE as Language) || "uk",
-};
-
-export const STORAGE_KEYS = {
-  BACKOFFICE_LANG: "backoffice_lang",
-  WEBSITE_LANG: "website_lang",
-};
--- src/shared/lib/i18n/index.ts
+++ src/shared/lib/i18n/index.ts
@@ -1,32 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { AuthRoutes } from "@/features/auth/api/routes.ts";
-import { DEFAULT_LANGUAGES, STORAGE_KEYS } from "@/shared/lib/constants.ts";
-
 import ruCommon from "./locales/ru/common.json";
 import ukCommon from "./locales/uk/common.json";
-
-const getInitialLanguage = () => {
-  const isBackoffice = window.location.pathname.startsWith(
-    AuthRoutes.backofficeRoot(),
-  );
-  const storageKey = isBackoffice
-    ? STORAGE_KEYS.BACKOFFICE_LANG
-    : STORAGE_KEYS.WEBSITE_LANG;
-  const defaultLang = isBackoffice
-    ? DEFAULT_LANGUAGES.backoffice
-    : DEFAULT_LANGUAGES.client;
-
-  return localStorage.getItem(storageKey) || defaultLang;
-};
 
 i18n.use(initReactI18next).init({
   resources: {
     ru: { common: ruCommon },
     uk: { common: ukCommon },
   },
-  lng: getInitialLanguage(),
+  lng: "ru",
   fallbackLng: "ru",
   ns: ["common"],
   defaultNS: "common",
